ab8d7c8a71bccc2aab6b9fffa7c8cf4841607011
s-matsubara <mtytsy15301217@gmail.com>
2022-04-19 10:52:14 +0900

    aaa

---
+++ js/oneday.js
@@ -0,0 +1,17 @@
+/*-------------------------------
+  資格取得制度 swiper
+-------------------------------*/
+const qualification_swiper = new Swiper(".qualification__container", {
+  spaceBetween: 44,
+  // ページネーション
+  pagination: {
+    el: ".qualification-pagination",
+    type: "progressbar",
+    // clickable: true,
+  },
+  // 前後の矢印
+  navigation: {
+    nextEl: ".qualification-button-next",
+    prevEl: ".qualification-button-prev",
+  },
+});
